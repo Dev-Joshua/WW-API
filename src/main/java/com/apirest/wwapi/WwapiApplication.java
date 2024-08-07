84275f92a3ecdc48cc227f96411aa9677028dda1
Joshua Daniel <joshua_dup@hotmail.com>
2024-08-07 18:09:39 -0500

    Inicio API de proyecto woofworkout

---
+++ src/main/java/com/apirest/wwapi/WwapiApplication.java
@@ -0,0 +1,13 @@
+package com.apirest.wwapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WwapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WwapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apirest/wwapi/WwapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apirest.wwapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WwapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
